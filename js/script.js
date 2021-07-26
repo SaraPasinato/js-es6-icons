9b30e6fd41a0d6c1b6a1eee685c55f7d183a59db
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-26 16:33:56 +0200

    fix css add filter

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /*
-! Milestone 1
+*! Milestone 1
 Partendo dalla seguente struttura dati , mostriamo in pagina tutte 
 le icone disponibili come da layout.
-!Milestone 2
+*!Milestone 2
 Coloriamo le icone per tipo
 !Milestone 3
 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
@@ -15,14 +15,16 @@ Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 
 
 /* //*************** functions utils ********************// */
-const renderCards =(cards,targetElement)=>{
-    let cardTamplate = "";
-    cards.forEach((icon,i) => {
+//? FUNZIONE: STAMPO IN PAGINA
 
-        const hasOffset = i%5===0 ? `offset-lg-1`:` `;
+const renderCards = (cards, targetElement) => {
+    let cardTamplate = "";
+    cards.forEach((icon, i) => {
+
+        const hasOffset = i % 5 === 0 ? `offset-lg-1` : ` `;
         cardTamplate += `
         <div class="col col-sm-4 col-lg-2 ${hasOffset}">
-           <div class="card rounded">
+           <div class="card">
                <div class="card-body text-center pt-4">
                    <a href="" class="${icon.type}"><i class="${icon.family} ${icon.prefix}${icon.name} ${icon.prefix}2x"></i></a>
                    <h3 class="h5 text-uppercase">${icon.name.toUpperCase()}</h3>
@@ -31,11 +33,28 @@ const renderCards =(cards,targetElement)=>{
         </div>   
         `;
     });
-    targetElement.innerHTML=cardTamplate;
+    targetElement.innerHTML = cardTamplate;
 };
-
 
 // ? STAMPO IN PAGINA
 
-const iconsSection=document.querySelector("#icons-section .row");
-renderCards(icons,iconsSection);
+const iconsSection = document.querySelector("#icons-section .row");
+renderCards(icons, iconsSection);
+
+
+//? Logica: Select 
+const selectField = document.getElementById("filter");
+
+selectField.addEventListener('change', () => {
+    const filterValue = selectField.value;
+
+    if (filterValue === "all") {
+        renderCards(icons, iconsSection);
+        return;
+    }
+    //? Filtro Dinamico per tipo
+    const filteredIcons = icons.filter((icon) => filterValue === icon.type);
+    renderCards(filteredIcons, iconsSection);
+});
+
+
